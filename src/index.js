4496b1f81123a7e792e0f45ddf6c72234ffc0d62
NoriTaka68 <said@evhr.net>
2022-01-23 12:25:02 +0100

    Work in Progress

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {FaListAlt,FaCheckSquare, FaPlusSquare, FaTrash } from "react-icons/fa";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  render() {
+    return (
+
+      <section id="todo">
+        <h1 classNameName="m-3">Liste de taches</h1>
+        <ul className="list-group m-3">
+          <li className="list-group-item d-flex align-items-center">Ranger la vaiselle
+            <button className="btn btn-sm ms-auto btn-outline-success">&#x2713;</button>
+          </li>
+          <li className="list-group-item d-flex align-items-center">Répondre appel d'offres
+            <button className="btn btn-sm ms-auto btn-outline-success">&#x2713;</button>
+          </li>
+          <li className="list-group-item d-flex align-items-center">signer contrat
+            <button className="btn btn-sm ms-auto btn-outline-success">&#x2713;</button>
+          </li>
+          <li className="list-group-item d-flex align-items-center">Ranger le salon
+            <button className="btn btn-sm ms-auto btn-outline-success">&#x2713;</button>
+          </li>
+
+        </ul>
+
+        <footer className="d-flex justify-content-between bg-secondary p-3" id="mainFooter">
+          <div className="btn-group">
+            <a href="#" className="btn btn-outilise-dark bg-light"><FaListAlt/></a>
+            <a href="#" className="btn btn-outilise-dark bg-light"><FaCheckSquare/></a>
+            <a href="#" className="btn btn-outilise-dark bg-light"><FaPlusSquare/></a>
+          </div>
+          <button className="btn btn-outilise-dark bg-light"><FaTrash/></button>
+        </footer>
+
+      </section>
+
+
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
